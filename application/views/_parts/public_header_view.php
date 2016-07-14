f7b77f2e6e4d417e6d76acd058e47c92123f8c7e
Angga <anggariskysetiawan@gmail.com>
2016-07-14 14:21:56 +0700

    penambahan initial scale responsive

--- application/views/_parts/public_header_view.php
+++ application/views/_parts/public_header_view.php
@@ -2,6 +2,8 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="icon" type="image/x-icon" href="<?php echo base_url();?>assets/public/themes/belancon/img/favicon.ico">
 	<?php
 		// Page Title
