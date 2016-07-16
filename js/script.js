187830a965b2ccca892f31803f799c72e6d526c4
maulayyacyber <ridaulmaulayya@gmail.com>
2016-07-16 08:10:11 +0700

    penambahan console message

--- js/script.js
+++ js/script.js
@@ -169,3 +169,9 @@ $(document).ready(function () {
 window.unload = function(e) {
   //Cart.clearAll();
 };
+
+"console" in window && console.log("%cHi Developer\nIf you find an error please report  on this page %chttps://dev.belancon.com/bug/\n%cGive your feedback on this page %chttps://dev.belancon.com/feedback/",
+  "color: #000000; font-size: 17px; line-height: 2",
+  "color: #2bbbad; font-size: 17px; line-height: 2; font-style: normal",
+  "color: #000000; font-size: 17px; line-height: 2",
+  "color: #2bbbad; font-size: 17px; line-height: 2; font-style: normal"); 
